f66b9d321407777fe318d1a110f99e7ab9d5e2d4
Manoj <manojkanumuri007@gmail.com>
2024-07-18 23:03:37 +0530

    UI and database done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import Searchbar from "@/components/Searchbar"
 import Image from "next/image"
-
-
-const Home = () => {
+import HeroCarousel from "@/components/HeroCarousel"
+import { getAllProducts, getSimilarProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
+const Home = async () => {
+  const allProducts = await getAllProducts();
+  
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-2 border-red-500">
+      <section className="px-6 md:px-20 py-24 border-2">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
@@ -24,9 +28,9 @@ const Home = () => {
             Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
             </p>
 
-            Search Bar
+            <Searchbar/>
           </div>
-          HeroCarousel
+          <HeroCarousel/>
         </div>
       </section>
 
@@ -34,9 +38,9 @@ const Home = () => {
         <h2 className="section-text">Trending</h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {['Apple Iphone 15', 'Book','Sneakers'].map
+          {allProducts?.map
           ((product)=>(
-            <div> {product} </div>
+            <ProductCard key={product._id} product={product}/>
           ))}
 
         </div>
---
+++ bugs.md
@@ -0,0 +1,2 @@
+1.Discount rate is not stored in DB
+2.reviewsCount is also not there in DB
